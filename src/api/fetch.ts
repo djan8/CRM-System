708740682bbcb945e3e9647299e77c8e33e574af
Sergey <faberdjan@gmail.com>
2026-02-25 19:32:54 +0300

    refactor function addTask, fixed block catch

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -54,8 +54,11 @@ export async function addTask(task: string) {
     const data = await res.json();
     console.log(data);
   } catch (err) {
-    console.error(err);
-    throw err;
+    // throw err;
+    if (err instanceof Error) {
+      alert(`${err.name}-${err.message}`);
+      alert(`Вы написали: ${task}`);
+    }
   }
 }
 
